c0670df863b57a5f4f2233bdf0c2037b81a7400b
aminaIsTrue <aamina.ouj@.com>
2021-04-27 19:02:25 +0200

    create Django project structure ==> MVT

---
+++ users/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+
+    return render (request, 'users/home.html')
+
+
+def register_view(request):
+    
+    return render (request, 'users/home.html')
