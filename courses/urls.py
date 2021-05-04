1f37a60e72fd5117661c3d6c8bf70828d6076c8b
aminaIsTrue <aamina.ouj@.com>
2021-05-04 21:38:41 +0200

    add Create update and delete method views and their templates

--- courses/urls.py
+++ courses/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
 
     path('', views.course_list, name = 'course-home-path'),
     path('course-detail/<int:pk>/', views.course_detail, name = 'course-detail-path'),
+    path('course-create/', views.create_course, name = 'course-create-path'),
+    path('course-update/<int:id>/', views.update_course, name = 'course-update-path'),
+    path('course-delete/<int:id>/', views.delete_course, name = 'course-delete-path'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Course
+from .forms import CreateCourseForm
 
 # Create your views here.
 
@@ -13,3 +14,43 @@ def course_detail(request, pk):
 
 
     return render (request, 'courses/course-detail.html', {'course' : Course.objects.get(id = pk)})
+
+
+# create a new course using django forms
+
+def create_course(request):
+
+    if request.method == 'POST':
+        obj = CreateCourseForm(request.POST, request.FILES)
+        #print ('#####################  before valid ###############  ' , obj.image.url )
+        if obj.is_valid():
+            obj = obj.save(commit = False)
+            obj.author = request.user
+            obj.save()
+            return redirect('course-home-path')
+
+    return render(request,'courses/create-course.html', {'form' : CreateCourseForm()})
+
+def update_course(request, id):
+    
+    if request.method == 'POST':
+        obj = CreateCourseForm(request.POST, request.FILES, instance = Course.objects.get(id = id))
+       
+        if obj.is_valid():
+            obj = obj.save(commit = False)
+            obj.author = request.user
+            obj.save()
+            return redirect('course-home-path')
+
+    return render(request,'courses/update-course.html', {'form' : CreateCourseForm(instance = Course.objects.get(id = id)),
+                                                            'course' : Course.objects.get(id = id) })
+
+
+def delete_course(request, id):
+    
+    obj = Course.objects.get(id = id)
+
+    if request.method == 'POST':       
+        obj.delete()
+        return redirect('course-home-path')
+    return render(request,'courses/delete-course.html')
