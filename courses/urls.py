03f20a56560662d4797f1379beb252a1453a118e
aminaIsTrue <aamina.ouj@.com>
2021-05-04 20:27:29 +0200

    add business logic for retrieve and detail views

--- courses/urls.py
+++ courses/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
 
-    path('', views.course_list, name = 'course-home-path')
+    path('', views.course_list, name = 'course-home-path'),
+    path('course-detail/<int:pk>/', views.course_detail, name = 'course-detail-path'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -7,3 +7,9 @@ def course_list(request):
 
 
     return render (request, 'courses/course-list.html', {'courses' : Course.objects.all()})
+
+
+def course_detail(request, pk):
+
+
+    return render (request, 'courses/course-detail.html', {'course' : Course.objects.get(id = pk)})
